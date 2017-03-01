8174128fb83a13431daf7c82a9b836607ecd00c6
[jsw324] <jwalkow@tallience.com>
2017-03-01 17:27:58 -0500

    Added styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ const React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 classname="text-center">About</h1>
+      <p>Use this app to get your local weather.  This is a learning project built on React</p>
+      <p>Find the source code <a href="https://github.com/jsw324/react-weather">here</a></p>
+      <p>Some of the tools used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - JavaScript framework
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Open Weather Map API
+        </li>
+        <li>
+          <a href="https://github.com/jsw324/react-weather">Github</a> - Github
+        </li>
+      </ul>
     </div>
   )
 };
