bed63170c45604dd2916020f3318c002b77ead33
[jsw324] <jwalkow@tallience.com>
2017-03-01 16:14:22 -0500

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3500;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 //Create our app
 var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3500, () => {
-  console.log('Express server is up on port 3500');
+app.listen(PORT, () => {
+  console.log('Express server is up on port 3500 ' + port);
 });
