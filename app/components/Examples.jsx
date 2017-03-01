853fd4b395a8a91bc13c1742251e100de4361602
[jsw324] <jwalkow@tallience.com>
2017-03-01 17:17:33 -0500

    Added styling with Foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,18 @@
 const React = require('react');
-
+const {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ul>
+        <li>
+          <Link to="/?location=Boca%20Raton">Boca Raton, Fl</Link>
+        </li>
+        <li>
+          <Link to="/?location=newyork">New York, NY</Link>
+        </li>
+      </ul>
     </div>
   )
 };
